95cde9f29ddd28dad4b3e46deee3a1f050acb96f
hooramseo <hooram.seo@lge.com>
2021-06-16 16:46:54 +0900

    Add postgresql access code
    :Detailed Notes:
    Add postgresql access code
    
    :Reference:

--- hoo_blog/server/server.js
+++ hoo_blog/server/server.js
@@ -2,12 +2,36 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5020;
 
+var {Client} = require('pg');
+
+const client = new Client({
+  user: "" /* Set Username */,
+  host: "" /* Set Host */,
+  database: "" /* Set Database */,
+  password: "" /* Set Password */,
+  port: "" /* Set Port */,
+});
+
+client.connect();
+
 app.get('/', (req, res) => {
     res.send("Welcome to Hoo's blog");
 })
 
 app.get('/api/host', (req, res) => {
-    res.send({ host : 'hooram2' });
+    res.send({ host : 'hooram23' });
+})
+
+app.get('/api/getInfo', (req, res) => {
+    client.query('SELECT * from testa', (err, data) => {
+        if (!err) {
+            res.send(data.rows);
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+        //client.end();
+    });
 })
 
 app.listen(PORT, () => {
--- hoo_blog/src/App.js
+++ hoo_blog/src/App.js
@@ -1,21 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const [host, setHost] = useState("default");
+  const [info, setInfo] = useState({});
+  const prevInfo = info;
+  console.log("info", info);
 
   const _getHost = async() => {
     const res = await axios.get('/api/host');
     setHost(res.data.host);
   }
 
+  const _getInfo = async() => {
+    const res = await axios.get('/api/getInfo');
+    
+    if ( JSON.stringify(prevInfo) !== JSON.stringify(res.data) ) {
+      setInfo(res.data);
+    }
+  };
+
   useEffect(() => {
     _getHost();
-  });
+    _getInfo();
+  }, [host, info]);
 
-  console.log(host);
+  //console.log(host, info);
 
   return (
     <div className="App">
@@ -31,10 +43,13 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React with {host}
+          {( info && Object.keys(info).length ) ? info[0].val2 : ''}
         </a>
       </header>
     </div>
   );
+
+  return null;
 }
 
 export default App;
