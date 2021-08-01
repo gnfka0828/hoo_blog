e38df6851d680f0ea1704dd1da190cc104548734
hooram.seo <gnfka0828@gmail.com>
2021-08-01 16:07:16 +0900

    Edit the Header link on the main page
    :Detailed Notes:
    1. 메인 페이지의 Hoolog 링크가 동작하지 않도록 함.
    
    :Reference:
    - div 태그가 선택되지 않게(블록 잡히지 않도록) 하는 방법
    https://mylife365.tistory.com/22

--- hoo_blog/src/component/Header/Header.js
+++ hoo_blog/src/component/Header/Header.js
@@ -3,7 +3,8 @@
 const Header = () => {
     return (
         <header id="header" className="alt">
-            <a href="index.html" className="logo"><strong>Hoolog</strong> <span>by hooram.seo</span></a>
+            {/* <a href="index.html" className="logo"><strong>Hoolog</strong> <span>by hooram.seo</span></a> */}
+            <div className="logo"><strong>Hoolog</strong> <span>by hooram.seo</span></div>
             {/* <Link to="/" className="logo"><strong>Hoolog</strong> <span>by hooram.seo</span></Link> */}
             <nav>
                 <a href="#menu">Menu</a>
